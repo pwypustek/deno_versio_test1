a0ebce71444645e05c7b720e3ebbc66a2217e05c
pwypustek <47745369+pwypustek@users.noreply.github.com>
2021-12-02 13:52:49 +0100

    Update test1.js

--- test1.js
+++ test1.js
@@ -3,11 +3,11 @@
  */
 import { serve } from "https://deno.land/std@0.92.0/http/server.ts";
 
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-//console.log("HTTP webserver running.  Access it at:  http://localhost:8080/");
+const server = serve({ hostname: "0.0.0.0", port: 80 });
+//console.log("HTTP webserver running.  Access it at:  http://localhost:80/");
 
 for await (const request of server) {
-  let bodyContent = "Your user-agent is:\n\n";
+  let bodyContent = "Your user-agent iiiiiiis:\n\n";
   bodyContent += request.headers.get("user-agent") || "Unknown";
 
   request.respond({ status: 200, body: bodyContent });
